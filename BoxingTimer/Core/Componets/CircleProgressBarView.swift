1b93239f4f5ffdab8b0ee5f7d3173954c92b8312
gias <khaitov.gias@gmail.com>
2024-02-21 14:48:17 +0500

    etension for Circle progress bar view UI

--- BoxingTimer/Core/Componets/CircleProgressBarView.swift
+++ BoxingTimer/Core/Componets/CircleProgressBarView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct CircleProgressBarView: View {
+    
+    let progress:CGFloat
+    
+    @State private var trimColor:LinearGradient = LinearGradient(colors: [Color.blue,Color.pink], startPoint: .bottomLeading, endPoint: .topTrailing)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundCircle
+            secondCircleWithTrim
+        }
+        .padding(30)
     }
 }
 
 #Preview {
-    CircleProgressBarView()
+    CircleProgressBarView(progress: 0.3)
+}
+
+extension CircleProgressBarView {
+    
+    private var backgroundCircle: some View {
+           Circle()
+               .fill(trimColor.opacity(0.2))
+               .stroke(.blue.opacity(0.30), style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+       }
+      
+    private var secondCircleWithTrim: some View {
+        Circle()
+            .trim(from: 0.0, to: progress)
+            .stroke(trimColor, style: StrokeStyle(lineWidth: 18, lineCap: .round, lineJoin: .round))
+            .rotationEffect(.degrees(-90))
+            .shadow(color: .blue.opacity(0.5), radius: 5)
+            .animation(.spring, value: progress)
+    }
 }

984feb9b28e03e65d12adbc16be4304578206e0d
gias <khaitov.gias@gmail.com>
2024-02-21 14:19:56 +0500

    Initial Commit

---
+++ BoxingTimer/BoxingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxingTimerApp.swift
+//  BoxingTimer
+//
+//  Created by ali on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoxingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
